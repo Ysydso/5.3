471467b7ee3df2eff0fa26e4ded386a05ab6a561
youri <124888543+SielStudent@users.noreply.github.com>
2023-11-29 13:11:00 +0100

    added exerciseService for api calls

---
+++ GymWorkDisclosedFront/gymworkdisclosedfront/app/components/services/exerciseService.js
@@ -0,0 +1,10 @@
+﻿export async function getExercises() {
+    const res = await fetch(`http://localhost:5206/api/Exercise`)
+    const data = await res.json()
+    return data
+}
+export async function getExerciseByGymGoerId(id) {
+    const res = await fetch(`http://localhost:5206//api/Exercise/GetExercisesByGymGoer/${id}`)
+    const data = await res.json()
+    return data
+}
